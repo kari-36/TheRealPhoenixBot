2996ec40568acc9270b620c727f1aa378a95f6df
Hell God <83143204+kari-36@users.noreply.github.com>
2021-10-18 12:22:53 +0500

    Update blacklistchats.py

--- tg_bot/modules/blacklistchats.py
+++ tg_bot/modules/blacklistchats.py
@@ -1,3 +1,29 @@
+"""
+MIT License
+
+Copyright (C) 2021 Slient-Boy
+
+This file is part of @ShokoGBot  (Elaina) [Telegram Bot]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from telegram import Bot, Update
 from telegram.ext import MessageHandler, Filters, DispatcherHandlerStop
 from telegram.error import TelegramError
@@ -14,7 +40,7 @@ def blacklist_chats(bot: Bot, update: Update):
         return
     try:
         chat.send_message(
-            "This chat has been blacklisted! Head over to @PhoenixSupport to find out why!"
+            "This chat has been blacklisted! Head over to @Elaina_Support_Chat to find out why!"
         )
         chat.leave()
         raise DispatcherHandlerStop
