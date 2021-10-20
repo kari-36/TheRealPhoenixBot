15710c0662c9500e61ee254ad9d96f0cad75d7da
Hell God <83143204+kari-36@users.noreply.github.com>
2021-10-20 12:44:31 +0500

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -34,14 +34,14 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, DispatcherHandlerStop, run_async
 from telegram.utils.helpers import escape_markdown
 
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import user_admin, connection_status
-from tg_bot.modules.helper_funcs.extraction import extract_text
-from tg_bot.modules.helper_funcs.filters import CustomFilters
-from tg_bot.modules.helper_funcs.misc import build_keyboard
-from tg_bot.modules.helper_funcs.string_handling import split_quotes, button_markdown_parser
-from tg_bot.modules.sql import cust_filters_sql as sql
+from Elaina import dispatcher, LOGGER
+from Elaina.modules.disable import DisableAbleCommandHandler
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.chat_status import user_admin, connection_status
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.extraction import extract_text
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.filters import CustomFilters
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.misc import build_keyboard
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.string_handling import split_quotes, button_markdown_parser
+from Elaina.modules.database_ptb_funcs.sql import cust_filters_sql as sql
 
 HANDLER_GROUP = 10
 BASIC_FILTER_STRING = "*Filters in this chat:*\n"
