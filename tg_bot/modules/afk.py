f109be48ccdddcb8d0be926bea6e9472c056dbee
Hell God <83143204+kari-36@users.noreply.github.com>
2021-10-20 12:17:03 +0500

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -30,10 +30,10 @@ from telegram import Message, Update, Bot, User
 from telegram import MessageEntity
 from telegram.ext import Filters, MessageHandler, run_async
 
-from tg_bot import dispatcher
-from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
-from tg_bot.modules.sql import afk_sql as sql
-from tg_bot.modules.users import get_user_id
+from Elaina import dispatcher
+from Elaina.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
+from Elaina.modules.database_ptb_funcs.sql import afk_sql as sql
+from Elaina.modules.users import get_user_id
 
 AFK_GROUP = 7
 AFK_REPLY_GROUP = 8
