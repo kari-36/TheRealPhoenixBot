c539f9f984431f98e35efb66e7b037047eb2d0b5
Hell God <83143204+kari-36@users.noreply.github.com>
2021-10-20 12:33:01 +0500

    Update blacklist.py

--- tg_bot/modules/blacklist.py
+++ tg_bot/modules/blacklist.py
@@ -32,12 +32,12 @@ from telegram import Message, Chat, Update, Bot, ParseMode
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
 
-import tg_bot.modules.sql.blacklist_sql as sql
-from tg_bot import dispatcher, LOGGER
-from tg_bot.modules.disable import DisableAbleCommandHandler
-from tg_bot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from tg_bot.modules.helper_funcs.extraction import extract_text
-from tg_bot.modules.helper_funcs.misc import split_message
+import Elaina.modules.database_ptb_funcs.sql.blacklist_sql as sql
+from Elaina import dispatcher, LOGGER
+from Elaina.modules.disable import DisableAbleCommandHandler
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.chat_status import user_admin, user_not_admin
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.extraction import extract_text
+from Elaina.modules.database_ptb_funcs.python_telegram_bot.misc import split_message
 
 BLACKLIST_GROUP = 11
 
